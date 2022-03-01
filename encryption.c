f15191b3907c6a55a396700f87939c67884ebc75
Lakshay <60613697+Lakshay0124@users.noreply.github.com>
2022-03-01 17:29:27 +0000

    Update encryption.c

--- encryption.c
+++ encryption.c
@@ -5,52 +5,37 @@ int encrypter(int option);
 int main(void)
 {
 
-	int option=get_int("What do you want to do 1.encrypt a message , 2.decrypt a message: ");
+    int option = get_int("What do you want to do 1.encrypt a message , 2.decrypt a message: ");
 
-
-	 encrypter(option);
-
+    encrypter(option);
 }
 int encrypter(int option)
-{	
-	if (option==1)
-	{
-		string message=get_string("What is your message: ");
-		int pass=get_int("What number key you want to use to encrypt the message: ");
-		for (int i=0; i<strlen(message); i++)
-		{
-			int b = message[i]-pass;
-			char f = (char) b ;
-			printf("%c" , f );
+{
+    if (option == 1)
+    {
+        string message = get_string("What is your message: ");
+        int pass = get_int("What number key you want to use to encrypt the message: ");
+        for (int i = 0; i < strlen(message); i++)
+        {
+            int b = message[i] - pass;
+            char f = (char)b;
+            printf("%c", f);
+        }
+        printf("\n");
+    }
 
+    else if (option == 2)
+    {
+        string message = get_string("What is your message: ");
+        int pass1 = get_int("What is the number key to decrypt this message: ");
 
-
-
-
-
-		}
-		printf("\n");
-
-		
-			
-	}
-
-	else if (option==2 )
-	{
-		string message=get_string("What is your message: ");
-		int pass1=get_int("What is the number key to decrypt this message: ");
-
-		for (int i=0; i<strlen(message); i++)
-		{
-			int c=message[i]+pass1;
-			char k = (char) c;
-			printf("%c" , k );
-		}
-		printf("\n");
-
-
-		
-	}
-	return 0;	
-	
+        for (int i = 0; i < strlen(message); i++)
+        {
+            int c = message[i] + pass1;
+            char k = (char)c;
+            printf("%c", k);
+        }
+        printf("\n");
+    }
+    return 0;
 }
